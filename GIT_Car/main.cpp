1dff27a82b7bc64e0a67fa84fe2870c2b5a37390
Denys <den.boiko2007.j@gmail.com>
2024-11-16 21:55:28 +0100

    third

--- GIT_Car/main.cpp
+++ GIT_Car/main.cpp
@@ -7,3 +7,13 @@ struct Car
 	int Cost;
 	char Country_producer[100];
 };
+struct Rectangle
+{
+	int Rectangle_l_1;
+	int Rectangle_l_2;
+};
+int main() {
+	std::cout << "Ich bin Denys ";
+
+	return 0;
+}
